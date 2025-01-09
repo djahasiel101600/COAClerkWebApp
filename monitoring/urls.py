cb17258294ab32886654bf58c9e981caf64b833e
djahasiel16 <djahasiel@gmail.com>
2025-01-09 08:08:00 +0800

    Added Update Page for IAR Records Model -  Monitoring App

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='MonitoringIndex')
+    path('', views.index, name='MonitoringIndex'),
+    path('update-iar/<int:pk>/edit/', views.update_iarrecord, name='update-iar'),
 ]
